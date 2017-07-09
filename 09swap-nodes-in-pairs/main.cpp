59ff2b54440f4509cf185d90ce44be346f79a135
landonguo <linuxwalkers@163.com>
2017-07-09 20:11:18 +0800

    begin 09,10 sync

---
+++ 09swap-nodes-in-pairs/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+struct ListNode {
+	int val;
+	ListNode *next;
+
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+//Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+class Solution {
+public:
+	ListNode *swapPairs(ListNode *head) {
+
+	}
+};
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
