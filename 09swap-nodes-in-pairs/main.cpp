6ad46b64cb72691f6e21b5bd50ee7c04b43e087d
linuxwalkers <linuxwalkers@163.com>
2017-07-10 21:22:59 +0800

    buggy 09 10 not pass, will fix it when idle

--- 09swap-nodes-in-pairs/main.cpp
+++ 09swap-nodes-in-pairs/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 
-
+using namespace std;
 struct ListNode {
 	int val;
 	ListNode *next;
-
 	ListNode(int x) : val(x), next(NULL) {}
 };
 
@@ -13,8 +12,34 @@ struct ListNode {
 class Solution {
 public:
 	ListNode *swapPairs(ListNode *head) {
+        if(head==NULL|| head->next==NULL)
+            return head;
+        ListNode *cur=head;
+        ListNode *precur=head;
+        ListNode *tmp=NULL;
+        ListNode *newhead=NULL;
 
-	}
+        while(cur!=NULL && cur->next!=NULL){
+            precur=cur;  //1
+            cur=cur->next;  //2
+            cur->next=precur;  //2->1
+            precur->next=tmp; //2->1->tmp
+
+
+            if(newhead==NULL)
+                newhead=cur;
+            else{
+                tmp=cur;
+            }
+            cur=cur->next;
+//             1 2 3     2 1
+//            ListNode *next=cur->next->next;   //3
+//            cur->next=precur;
+
+        }
+        return newhead;
+
+    }
 };
 
 int main() {
--- 10add-two-numbers/main.cpp
+++ 10add-two-numbers/main.cpp
@@ -2,19 +2,47 @@
 
 
 struct ListNode {
-	int val;
-	ListNode *next;
-	ListNode(int x) : val(x), next(NULL) {}
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {}
 };
 
 class Solution {
 public:
-	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        int sum = 0;
+        ListNode *head = NULL;
+        ListNode *tail = NULL;
 
-	}
+        while (l1 != NULL || l2 != NULL) {
+            if (l1 != NULL) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != NULL) {
+                sum += l2->val;
+                l1 = l2->next;
+            }
+            ListNode *newnode = new ListNode(sum % 10);
+            sum = sum / 10;
+            if (head == NULL) {
+                head = newnode;
+                tail=head;
+            } else {
+                tail->next = newnode;
+                tail = newnode;
+            }
+
+        }
+        if (sum > 0) {
+            tail->next = new ListNode(sum);
+        }
+        return head;
+    }
 };
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
