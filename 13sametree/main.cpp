0a1e929a9f80d8fd42b6bb75da978bd9a59bdf35
linuxwalkers <linuxwalkers@163.com>
2017-07-13 04:10:21 +0800

    finish 13sametree and 14max-depth using recursive

---
+++ 13sametree/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL && q==NULL)
+            return true;
+        if((p==NULL && q!=NULL) || (p!=NULL && q==NULL))
+            return false;
+        if(p->val==q->val){
+            return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        }else{
+            return false;
+        }
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
