6fa094125792026d46212290f31894e40bbb3746
linuxwalkers <linuxwalkers@163.com>
2017-07-16 00:37:23 +0800

    sync 18 minheight running error. fix tomorrow, remain 12 19 20

--- 18minheight/main.cpp
+++ 18minheight/main.cpp
@@ -1,18 +1,52 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <queue>
 using namespace std;
 
-struct UndirectedGraphNode {
-    int label;
-    vector<UndirectedGraphNode *> neighbors;
-    UndirectedGraphNode(int x) : label(x) {};
-};
+//How to represent a graph
+//vector<set<int>> graph;
+//for(auto edge:edges){
+//graph[edge.first].insert(edge.second);
+//graph[edge.second].insert(edge.first);
+//}
 
-
+// remove leaves layer-by-layer
+//The basic idea is "keep deleting leaves layer-by-layer, until reach the root."
 class Solution {
 public:
-    vector<int> findMinHeightTrees(int n, vector<pair<int, int>>& edges) {
+    vector<int> findMinHeightTrees(int n, vector<pair<int, int>> &edges) {
+        //shortest distance to the leaf node  BFS
+        vector<set<int>> graph;
+        for (auto edge:edges) {
+            graph[edge.first].insert(edge.second);
+            graph[edge.second].insert(edge.first);
+        }
+        if (n == 1)
+            return {0};
 
+        vector<int> leafnodes;
+        vector<int> nextleaves;
+        for (int i = 0; i < graph.size(); i++) {
+            if (graph[i].size() == 1)
+                leafnodes.push_back(i);  //all leaf node
+        }
+        while(1) {
+            for (auto leafnode: leafnodes) {
+                for (auto neighbor: graph[leafnode]) {
+                    graph[neighbor].erase(leafnode);
+                    if (graph[neighbor].size() == 1) //leaf mode again
+                        nextleaves.push_back(neighbor);
+                }
+            }
+            if(nextleaves.empty())
+                return nextleaves;
+            leafnodes = nextleaves;
+        }
     }
 };
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
