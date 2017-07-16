967fa92a1902eb76fb8d98d5aa1a3d8c3c6c1bde
landonguo <linuxwalkers@163.com>
2017-07-16 21:33:54 +0800

    finish reconstruct-itinerary oula line

--- 19reconstruct-itinerary/main.cpp
+++ 19reconstruct-itinerary/main.cpp
@@ -1,4 +1,53 @@
 #include <iostream>
+#include <string>
+#include <set>
+#include <vector>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+//欧拉一笔画问题
+class Solution {
+public:
+    map<string,multiset<string>> graph;
+    vector<string> result;
+    vector<string> findItinerary(vector<pair<string, string>> tickets) {
+
+        for (auto edge:tickets) {
+            graph[edge.first].insert(edge.second);
+        }
+//        DFS("JFK");
+        stack<string> waystack;
+        waystack.push("JFK");
+        while(!waystack.empty()){
+            string current=waystack.top();
+            if(graph[current].empty()){
+                result.push_back(current);
+                waystack.pop();
+
+            }
+            else{
+                waystack.push(*(graph[current].begin()));
+                graph[current].erase(graph[current].begin());
+            }
+        }
+
+        reverse(result.begin(),result.end());
+        return result;
+    }
+
+//    void DFS(string departure)
+//    {
+//        while(graph[departure].size() > 0)
+//        {
+//            string tem = *graph[departure].begin();
+//            graph[departure].erase(graph[departure].begin());
+//            DFS(tem);
+//        }
+//        result.push_back(departure);
+//    }
+};
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
