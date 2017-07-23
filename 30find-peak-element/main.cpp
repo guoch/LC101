7a35342202fc634a73806cf773b445e7e40e9b07
landonguo <linuxwalkers@163.com>
2017-07-23 10:57:51 +0800

    commit 30 will use binary later

--- 30find-peak-element/main.cpp
+++ 30find-peak-element/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
 //https://leetcode.com/problems/find-peak-element/
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int len=nums.size();
+        if(len<=1)
+            return 0;
+        if(nums[0]>nums[1])
+            return 0;
+        for(int i=1;i<len-1;i++){
+            if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
+                return i;
+            }
+            i++;
+        }
+        if(nums[len-1]>nums[len-2])
+            return len-1;
+    }
+};
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<int> nums={1,2,3,1};
+    Solution solve1;
+    int res=solve1.findPeakElement(nums);
+    std::cout << res<< std::endl;
     return 0;
 }
