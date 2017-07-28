666048ac7eb0b3a4a619768e2b65a6f5543bdace
landonguo <linuxwalkers@163.com>
2017-07-28 21:21:26 +0800

    begin 42-44

---
+++ 42 01matrix/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
+
+
+    }
+
+    void bfs(vector<vector><int>& matrix){
+
+    }
+};
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
