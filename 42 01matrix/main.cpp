ab00f9f96bc39d289b9cee5146a44dec2baadc49
landonguo <linuxwalkers@163.com>
2017-07-29 21:02:56 +0800

    finish 43 finish 42 using coldclutter's way

--- 42 01matrix/main.cpp
+++ 42 01matrix/main.cpp
@@ -1,14 +1,63 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+
 using namespace std;
+
 class Solution {
+
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
+    vector<int> dx = {1, 0, -1, 0};
+    vector<int> dy = {0, 1, 0, -1};
+
+    class Node {
+    public:
+        int x;
+    public:
+        int y;
+    public:
+        int d;
+
+    public:
+        Node(int x, int y, int d) {
+            this->x = x;
+            this->y = y;
+            this->d = d;
+        }
+    };
 
 
-    }
+    vector<vector<int>> updateMatrix(vector<vector<int>> &matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> result(n, vector<int>(m, 0));
+        vector<vector<bool>> mark(n, vector<bool>(m, false));
+        queue<Node> myqueue;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    myqueue.push(Node(i, j, 0));
+                    mark[i][j] = true;
+                }
 
-    void bfs(vector<vector><int>& matrix){
+            }
+        }
+        while (!myqueue.empty()) {
+            Node node = myqueue.front();
+            myqueue.pop();
+            result[node.x][node.y] = node.d;
+            for (int k = 0; k < 4; k++) {
+                int x = node.x + dx[k];
+                int y = node.y + dy[k];
+                if (0 <= x && x < n && 0 <= y && y < m && !mark[x][y]) {
+                    myqueue.push(Node(x, y, node.d + 1));
+                    mark[x][y] = true;
+                }
+            }
+
+        }
+        return result;
+
 
     }
 };
--- 43letter-combinations/main.cpp
+++ 43letter-combinations/main.cpp
@@ -1,4 +1,41 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
+        if (digits == "")
+            return result;
+
+        result.push_back("");
+
+        vector<string> dict(10);
+        dict[2] = "abc";
+        dict[3] = "def";
+        dict[4] = "ghi";
+        dict[5] = "jkl";
+        dict[6] = "mno";
+        dict[7] = "pqrs";
+        dict[8] = "tuv";
+        dict[9] = "wxyz";
+        for (int i = 0; i < digits.size(); i++) {
+            int size = result.size();
+            for (int j = 0; j < size; j++) {
+                string cur = result[0];
+                result.erase(result.begin());
+                for (int k = 0; k < dict[digits[i] - '0'].size(); k++) {
+                    result.push_back(cur + dict[digits[i] - '0'][k]);
+                }
+            }
+        }
+        return result;
+
+    }
+};
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
