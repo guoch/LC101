4fca4bffbacfa939bb14a474b234afe2627fa67f
landonguo <linuxwalkers@163.com>
2017-08-05 20:18:34 +0800

    thinking

--- 55word-break/main.cpp
+++ 55word-break/main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        vector<int> dp(s.size()+1);
-        int n=s.size();
-        for(int i=n-1;i>=0;i++){
+        vector<int> dp(s.size(),0);
+        for(int i=0;i<s.size();i++){
+            if(find(wordDict.begin(),wordDict.end(),s.substr(i,s.size()-i))){
+
+            }
+
 
         }
+
 
     }
 };
