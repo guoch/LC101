f8b896d3138256ed2f790d896179d5d377e45b8b
linuxwalkers <linuxwalkers@163.com>
2017-08-07 20:46:57 +0800

    finish 57edit-distance,begin 58

---
+++ 58distinct-subsequences/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+
+    }
+};
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
