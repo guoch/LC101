1baa9232be858b990fe6bba8e67e5edc7fe3160f
landonguo <linuxwalkers@163.com>
2017-08-17 20:34:14 +0800

    commit decode ways

--- 53decode-ways/main.cpp
+++ 53decode-ways/main.cpp
@@ -8,21 +8,20 @@ class Solution {
 public:
     int numDecodings(string s) {
         int n=s.size();
-        if(n==0)
+        if(n==0 || n>1 && s[0]=='0')
             return 0;
-        vector<int> dp(n+1);
+        vector<int> dp(n+1,0);
         dp[0]=1;
 //        int p=1,q=1;  //climb
-        for(int i=1;i<n;i++){
-            if(s[i]=='0')
-            if(s[i-1]=='1' || s[i-1]=='2' && s[i]<='6'){
+        for(int i=1;i<=n;i++){
+            dp[i]=(s[i-1]=='0')?0:dp[i-1];
+            if(i>1 && s[i-2]=='1' || s[i-2]=='2' && s[i-1]<='6'){
+                dp[i]+=dp[i-2];
                 //may 2 numbers
-            } else{
-
             }
 
-
         }
+        return dp[n];
 
     }
 };
